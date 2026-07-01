4dce97547ec52b234198ccf705b3943210c24d80
nosvasedis <nvasedis@gmail.com>
2026-07-01 12:59:25 +0300

    refactor: fix formatting of the below prop in OffersPage component

--- components/OffersPage.tsx
+++ components/OffersPage.tsx
@@ -884,8 +884,7 @@ export default function OffersPage({ products, materials, settings, collections,
                         <Plus size={20} /> Νέα Προσφορά
                     </button>
                 )}
-            />
-            below={(
+                below={(
               <div className="space-y-4">
                 <div className="rounded-xl border border-slate-100 bg-slate-50/50 p-3 shadow-sm">
                   <DebouncedSearchInput
